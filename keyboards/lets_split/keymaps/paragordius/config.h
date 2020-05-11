d6e9c2bc9414440552344b67a47b3f308dfb5fb4
FrobozzElectric <gpfau7@gmail.com>
2020-05-11 19:25:11 -0400

    changes to work with qwiic pro micro

--- keyboards/lets_split/keymaps/paragordius/config.h
+++ keyboards/lets_split/keymaps/paragordius/config.h
@@ -31,6 +31,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define USE_I2C
 
 /* Select hand configuration */
-#define MASTER_LEFT
+// #define MASTER_LEFT
 // #define MASTER_RIGHT
-// #define EE_HANDS
+#define EE_HANDS
+
+// fucking Qwiic Pro Micro
+#define SPLIT_USB_DETECT
