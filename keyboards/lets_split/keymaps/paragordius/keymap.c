8cf67e542f69fbd69c6431fd930428f954d4b4fe
FrobozzElectric <gpfau7@gmail.com>
2020-05-11 22:11:55 -0400

    unicode fix

--- keyboards/lets_split/keymaps/paragordius/keymap.c
+++ keyboards/lets_split/keymaps/paragordius/keymap.c
@@ -13,8 +13,8 @@ enum custom_keycodes {
 };
 
 /* Main `LOWER` and `RAISE` buttons are tap toggle */
-#define LOWER   TT(_LOWER)
-#define RAISE   TT(_RAISE)
+#define LOWER   OSL(_LOWER)
+#define RAISE   OSL(_RAISE)
 /* Corner `LWR_OS` and `RAS_OS` are the same, but are one-shot instead of tap toggle */
 #define LWR_OS  OSL(_LOWER)
 #define RAS_OS  OSL(_RAISE)
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_ortho_4x12(
   KC_ESC,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-  KC_TILD, X(CHCK), X(DAGG), X(IPCT), X(FING), X(ELIP),    X(ENDA), KC_MINUS,KC_EQL,  KC_LCBR, KC_LCBR, KC_PIPE,
+  KC_TILD, X(CHCK), X(DAGG), X(IPCT), X(FING), X(ELIP),    X(ENDA), KC_MINUS,KC_EQL,  KC_LCBR, KC_RCBR, KC_PIPE,
   _______, X(CROS), X(DDAG), X(BULL), X(REFR), X(INBG),    X(EMDA), KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, _______,
   _______, _______, _______, _______, _______, _______,    _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
@@ -92,18 +92,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  ,-----------------------------------------------------.  ,-----------------------------------------------------.
  | Esc    |   1    |   2    |   3    |   4    |   5    |  |   6    |   7    |   8    |   9    |   0    |        |
  |--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
- |   `    |  F1    |  F2    |  F3    |  F4    |  F5    |  | Left   | Down   | Up     | Right  |        |  \     |
+ |   `    |  F1    |  F2    |  F3    |  F4    |  F5    |  | Left   | Down   | Up     | Right  | UC_LNX |  \     |
  |--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
- |        |  F6    |  F7    |  F8    |  F9    |  F10   |  |  F11   |  F12   |        |        |        |        |
+ |        |  F6    |  F7    |  F8    |  F9    |  F10   |  |  F11   |  F12   |        |        | UC_MAC |        |
  |--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
  |        |        |        |        |        |        |  |        |        | Next   | Vol-   | Vol+   | Play   |
  `-----------------------------------------------------'  `-----------------------------------------------------'
  */
 [_RAISE] = LAYOUT_ortho_4x12(
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-  KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, KC_BSLS,
-  _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,     KC_F11,  KC_F12,  _______, _______, _______, _______,
+  KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,UC_M_LN, KC_BSLS,
+  _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,     KC_F11,  KC_F12,  _______, _______, UC_M_MA, _______,
   _______, _______, _______, _______, _______, _______,    _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 };
+
+void eeconfig_init_user(void) {
+    set_unicode_input_mode(UC_LNX);
+}
