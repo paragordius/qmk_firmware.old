d263cb737635b4285b903b4acfcc24dd7ed84993
FrobozzElectric <gpfau7@gmail.com>
2020-07-03 23:26:47 -0400

    sticky things

--- keyboards/crkbd/keymaps/paragordius/config.h
+++ keyboards/crkbd/keymaps/paragordius/config.h
@@ -24,16 +24,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
+// #define MASTER_LEFT
 // #define MASTER_RIGHT
-// #define EE_HANDS
+#define EE_HANDS
 
 #define SSD1306OLED
 
 #define USE_SERIAL_PD2
 
-#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+// #define TAPPING_FORCE_HOLD
+// #define TAPPING_TERM 100
 
 #define ONESHOT_TAP_TOGGLE 2
 #define TAPPING_TOGGLE 2
