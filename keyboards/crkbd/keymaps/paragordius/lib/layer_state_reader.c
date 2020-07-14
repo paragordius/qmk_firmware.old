d1f2493d679ed19a985c0bcccb2350a23788d066
FrobozzElectric <gpfau7@gmail.com>
2020-07-14 11:37:38 -0400

    ...

--- keyboards/crkbd/keymaps/paragordius/lib/layer_state_reader.c
+++ keyboards/crkbd/keymaps/paragordius/lib/layer_state_reader.c
@@ -1,16 +1,15 @@
-
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 #include "crkbd.h"
 
 // in the future, should use (1U<<_LAYER_NAME) instead, but needs to be moved to keymap,c
 #define L_BASE 0
-#define L_LOWER 2
-#define L_RAISE 4
-#define L_STICKY 6
-#define L_STICKY_2 8
-#define L_LOWER_STICKY 10
-#define L_RAISE_STICKY 12
+#define L_STICKY 2
+#define L_LOWER 4
+#define L_RAISE 8
+#define L_LOWER_STICKY 6
+#define L_RAISE_STICKY 10
+#define L_STICKY_STICKY 12
 #define L_CLEAR 14
 
 char layer_state_str[24];
@@ -30,7 +29,7 @@ const char *read_layer_state(void) {
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
     break;
   case L_STICKY:
-  case L_STICKY_2:
+  case L_STICKY_STICKY:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Sticky");
     break;
   case L_CLEAR:
