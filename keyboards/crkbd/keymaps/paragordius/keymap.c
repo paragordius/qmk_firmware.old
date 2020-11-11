e01adac0f500f3bba00074216a25311ebb20b5d7
FrobozzElectric <gpfau7@gmail.com>
2020-11-11 14:19:30 -0500

    ...

--- keyboards/crkbd/keymaps/paragordius/keymap.c
+++ keyboards/crkbd/keymaps/paragordius/keymap.c
@@ -144,37 +144,40 @@ const char *read_timelog(void);
 char mods_str[21] = {};
 
 const char *oneshot_mods_status(uint8_t mods, uint8_t locked_mods) {
-  char ctrl_status = ' ';
-  char shft_status = ' ';
-  char lgui_status = ' ';
-  char lalt_status = ' ';
-  // If there are active mods, denote them with lowercase
-  if (mods & MOD_MASK_CTRL) {
-    ctrl_status = 'c';
+  snprintf(mods_str, sizeof(mods_str), " ");
+  if (layer_state_is(_STICKY)) {
+    char ctrl_status = ' ';
+    char shft_status = ' ';
+    char lgui_status = ' ';
+    char lalt_status = ' ';
+    // If there are active mods, denote them with lowercase
+    if (mods & MOD_MASK_CTRL) {
+      ctrl_status = 'c';
+    }
+    if (mods & MOD_MASK_SHIFT) {
+      shft_status = 's';
+    }
+    if (mods & MOD_MASK_GUI) {
+      lgui_status = 'm';
+    }
+    if (mods & MOD_MASK_ALT) {
+      lalt_status = 'a';
+    }
+    // And if there are active locked mods, denote those with uppercase
+    if (locked_mods & MOD_MASK_CTRL) {
+      ctrl_status = 'C';
+    }
+    if (locked_mods & MOD_MASK_SHIFT) {
+      shft_status = 'S';
+    }
+    if (locked_mods & MOD_MASK_GUI) {
+      lgui_status = 'm';
+    }
+    if (locked_mods & MOD_MASK_ALT) {
+      lalt_status = 'A';
+    }
+    snprintf(mods_str, sizeof(mods_str), "Mods:[%c] [%c] [%c] [%c]", ctrl_status, shft_status, lgui_status, lalt_status);
   }
-  if (mods & MOD_MASK_SHIFT) {
-    shft_status = 's';
-  }
-  if (mods & MOD_MASK_GUI) {
-    lgui_status = 'g';
-  }
-  if (mods & MOD_MASK_ALT) {
-    lalt_status = 'a';
-  }
-  // And if there are active locked mods, denote those with uppercase
-  if (locked_mods & MOD_MASK_CTRL) {
-    ctrl_status = 'C';
-  }
-  if (locked_mods & MOD_MASK_SHIFT) {
-    shft_status = 'S';
-  }
-  if (locked_mods & MOD_MASK_GUI) {
-    lgui_status = 'G';
-  }
-  if (locked_mods & MOD_MASK_ALT) {
-    lalt_status = 'A';
-  }
-  snprintf(mods_str, sizeof(mods_str), "Mods:[%c] [%c] [%c] [%c]", ctrl_status, shft_status, lgui_status, lalt_status);
   return mods_str;
 }
 
